fc47572f1c6a5296fe158ba7cba4735bf778d2a6
Aldrin <cleetusaldrin30@gmail.com>
2022-12-27 14:50:45 +0530

    like working

--- Simple Extension/content.js
+++ Simple Extension/content.js
@@ -1,68 +1,18 @@
-const getDetails = ()=>{
-    let userName = document.querySelector('h1').innerHTML.replace(/\s/g, '')
-    let place = document.getElementsByClassName('pv-text-details__left-panel mt2')[0].querySelector("span").innerHTML.replace(/\s/g, '')
-    
-    return {
-        userName,
-        place
-    }
+// inside the page script
+console.log("I am in")
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(request.likeCount)
+  likeRandomPost(request.likeCount)
+
+});
+
+const likeRandomPost = (count)=>{
+  for (let index = 0; index < 3; index++) {
+    let posts = document.getElementsByClassName('react-button__trigger')
+    const randomIndex = Math.floor(Math.random() * posts.length)
+    const post = posts[index]
+    //posts = posts.splice(randomIndex,1)
+    post.click()
+  }
 }
-
-
-const getContactInfo = ()=>{
-    
-    return new Promise((resolve,reject)=>{
-
-    
-    document.getElementById("top-card-text-details-contact-info").click()
-
-
-    
-    let contactInfo = {
-        linkedIn: "",
-        email: "",
-    }
-
-    setTimeout(()=>{
-        try {
-            // Get Data
-            contactInfo.linkedIn = document.getElementsByClassName("artdeco-modal__content")[0].getElementsByClassName("pv-contact-info__ci-container")[0].querySelector("a").innerHTML.replace(/\s/g, '')
-            contactInfo.email = document.getElementsByClassName("artdeco-modal__content")[0].getElementsByClassName("pv-contact-info__ci-container")[1].querySelector("a").innerHTML.replace(/\s/g, '')
-
-            // Return Data
-            resolve(contactInfo)
-
-            // Close the Modal
-            document.getElementsByClassName("artdeco-modal__dismiss")[0].click()
-        } catch (error) {
-            document.getElementsByClassName("artdeco-modal__dismiss")[0].click()
-            reject(contactInfo) 
-        }
-      
-    },5000)
-
-})}
-
-
-const logUserData = async ()=>{
-
-    const contacts = await getContactInfo()
-        .then(data => {return data})
-        .catch(error => {return error})
-
-    const userInfo = {
-            name: getDetails().userName,
-            place: getDetails().place,
-            contactDetails: contacts
-        }
-
-    console.log(userInfo)
-
-}
-
-logUserData()
-
-
-
-
-  
--- Simple Extension/index.js
+++ Simple Extension/index.js
@@ -13,80 +13,127 @@
 // titleButton.onclick = getTitle
 
 // This is the main function that runs when the extension is activated
-function main() {
-    // Add the input fields to the pop-up window
-    var likeCountInput = document.createElement("input");
-    likeCountInput.setAttribute("type", "text");
-    likeCountInput.setAttribute("id", "like-count");
-    likeCountInput.setAttribute("placeholder", "Like count");
-    document.body.appendChild(likeCountInput);
+// function main() {
+//     // Add the input fields to the pop-up window
+//     var likeCountInput = document.createElement("input");
+//     likeCountInput.setAttribute("type", "text");
+//     likeCountInput.setAttribute("id", "like-count");
+//     likeCountInput.setAttribute("placeholder", "Like count");
+//     document.body.appendChild(likeCountInput);
     
-    var commentCountInput = document.createElement("input");
-    commentCountInput.setAttribute("type", "text");
-    commentCountInput.setAttribute("id", "comment-count");
-    commentCountInput.setAttribute("placeholder", "Comment count");
-    document.body.appendChild(commentCountInput);
+//     var commentCountInput = document.createElement("input");
+//     commentCountInput.setAttribute("type", "text");
+//     commentCountInput.setAttribute("id", "comment-count");
+//     commentCountInput.setAttribute("placeholder", "Comment count");
+//     document.body.appendChild(commentCountInput);
     
-    // Add the button to the pop-up window
-    var button = document.createElement("button");
-    button.innerHTML = "Like posts";
-    button.setAttribute("id", "like-button");
-    button.setAttribute("disabled", "true");
-    document.body.appendChild(button);
+//     // Add the button to the pop-up window
+//     var button = document.createElement("button");
+//     button.innerHTML = "Like posts";
+//     button.setAttribute("id", "like-button");
+//     button.setAttribute("disabled", "true");
+//     document.body.appendChild(button);
     
-    // Enable the button when both input fields have been filled in
-    likeCountInput.addEventListener("input", function() {
-      if (likeCountInput.value && commentCountInput.value) {  } else {
-        button.setAttribute("disabled", "true");
-      }
-    });
-    commentCountInput.addEventListener("input", function() {
-      if (likeCountInput.value && commentCountInput.value) {
-        button.removeAttribute("disabled");
-      } else {
-        button.setAttribute("disabled", "true");
-      }
-    });
+//     // Enable the button when both input fields have been filled in
+//     likeCountInput.addEventListener("input", function() {
+//       if (likeCountInput.value && commentCountInput.value) {  } else {
+//         button.setAttribute("disabled", "true");
+//       }
+//     });
+//     commentCountInput.addEventListener("input", function() {
+//       if (likeCountInput.value && commentCountInput.value) {
+//         button.removeAttribute("disabled");
+//       } else {
+//         button.setAttribute("disabled", "true");
+//       }
+//     });
     
-    // When the button is clicked, inject the content script into the LinkedIn Feed page and send a message to the content script
-    button.addEventListener("click", function() {
-      var likeCount = document.getElementById("like-count").value;
+//     // When the button is clicked, inject the content script into the LinkedIn Feed page and send a message to the content script
+//     button.addEventListener("click", function() {
+//       var likeCount = document.getElementById("like-count").value;
       
-      // Inject the content script into the LinkedIn Feed page
-      chrome.tabs.update({url: "https://www.linkedin.com/feed/"});
+//       // Inject the content script into the LinkedIn Feed page
+//       chrome.tabs.update({url: "https://www.linkedin.com/feed/"});
 
       
       
-      // Send a message to the content script with the number of likes to perform
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabss) {
+//       // Send a message to the content script with the number of likes to perform
+//       chrome.tabs.query({active: true, currentWindow: true}, function(tabss) {
 
         
 
-        chrome.tabs.onUpdated.addListener((tabId,changeInfo,tabs)=>{
+//         chrome.tabs.onUpdated.addListener((tabId,changeInfo,tabs)=>{
 
-            if(changeInfo.status === "complete"){
-
-        
-                if(tabs.url.includes("linkedin.com/feed/")){
-                    console.log("it is the tab")
-                    chrome.scripting.executeScript({
-                        files: ['content.js'],
-                        target: {tabId: tabId}
-                    })
-                    setTimeout(() => {
-                        chrome.tabs.sendMessage(tabId, {type: "likePosts", likeCount: likeCount});
-                    }, 2000);
-                } 
-
-            }
-        })
-
+//             if(changeInfo.status === "complete"){
 
         
+//                 if(tabs.url.includes("linkedin.com/feed/")){
+//                     console.log("it is the tab")
+//                     chrome.scripting.executeScript({
+//                         files: ['content.js'],
+//                         target: {tabId: tabId}
+//                     })
+//                     setTimeout(() => {
+//                         chrome.tabs.sendMessage(tabId, {type: "likePosts", likeCount: likeCount});
+//                     }, 2000);
+//                 } 
+
+//             }
+//         })
+
+
+
+
+const likeCount = document.getElementById("like")
+const comment = document.getElementById('comment')
+const submit = document.getElementById("titleButton")
         
-      });
-    });
+const toLinkedIn = ()=>{
+  chrome.tabs.update({url: 'https://www.linkedin.com/feed/'})
+
+  chrome.tabs.onUpdated.addListener((tabId,changeInfo,tab)=>{
+
+    if(changeInfo.status === "complete"){
+
+        if(tab.url.includes("linkedin.com/feed/")){
+            
+            chrome.scripting.executeScript({
+                files: ['content.js'],
+                target: {
+                    tabId: tabId
+                }
+            })
+
+
+            
+            // Send the amount of likes
+
+            chrome.tabs.sendMessage(tabId, {"likeCount": likeCount.value})
+
+        } 
+    }
+})
+
+}
+
+
+
+
+submit.onclick = toLinkedIn
+
+likeCount.addEventListener('input', ()=>{
+  if(likeCount.value && comment.value){
+    submit.removeAttribute('disabled')
+  }else{
+    submit.setAttribute("disabled", "true")
   }
-  
-  // Run the main function when the extension is activated
-  main();
+})
+
+comment.addEventListener('input', ()=>{
+  if(likeCount.value && comment.value){
+    submit.removeAttribute('disabled')
+  }else{
+    submit.setAttribute("disabled", "true")
+  }
+})
+
