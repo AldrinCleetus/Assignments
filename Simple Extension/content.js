c4234019cf7cf3ee1b06d040db21429860909d5c
Aldrin <cleetusaldrin30@gmail.com>
2022-12-27 16:25:29 +0530

    final

--- Simple Extension/content.js
+++ Simple Extension/content.js
@@ -1,18 +1,44 @@
-// inside the page script
-console.log("I am in")
+const likeAndCommentButtons = document.getElementsByClassName('feed-shared-social-actions')
+
+let posts = []
+
+for (let index = 0; index < likeAndCommentButtons.length; index++) {
+  const element = likeAndCommentButtons[index].getElementsByClassName('react-button__trigger')[0]
+  const element2 = likeAndCommentButtons[index].getElementsByClassName('comment-button')[0]
+  posts.push({
+    "like":element,
+    "comment": element2
+  }) 
+}
+console.log(posts)
+
+const likeAndCommentRandomPosts = (count,commentToWrite)=>{
+
+  if(count > posts.length){
+    count = posts.length
+  }
+
+  for (let index = 0; index < count; index++) {
+
+    const post = posts[index].like
+    const comment = posts[index].comment
+
+    // like
+    post.click()
+    comment.click()
+
+    document.getElementsByClassName('comments-comment-box-comment__text-editor')[index].getElementsByTagName('p')[0].innerHTML = commentToWrite
+    setTimeout(() => {
+      document.getElementsByClassName('comments-comment-box__submit-button')[0].click()
+    }, 2000);
+
+  }
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log(request.likeCount)
-  likeRandomPost(request.likeCount)
+  console.log(request.comment)
+  likeAndCommentRandomPosts(request.likeCount,request.comment)
 
 });
 
-const likeRandomPost = (count)=>{
-  for (let index = 0; index < 3; index++) {
-    let posts = document.getElementsByClassName('react-button__trigger')
-    const randomIndex = Math.floor(Math.random() * posts.length)
-    const post = posts[index]
-    //posts = posts.splice(randomIndex,1)
-    post.click()
-  }
-}
